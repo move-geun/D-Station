9349f23cf5f6de799bdbf461279e6e5fcbe823d6
MinJi Jeong <70947171+minzzz1st7@users.noreply.github.com>
2022-11-07 20:21:00 +0900

    add: 깃헙 정보 받기(회원가입)

--- frontend/src/pages/auth/SignupPage.jsx
+++ frontend/src/pages/auth/SignupPage.jsx
@@ -2,75 +2,51 @@ import React from "react";
 import auth_axios from "../../api/user";
 import { useLocation, useNavigate } from "react-router-dom";
 import {Container, InputNickname} from './SignupPage.style';
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import SignupLevel1 from "../../components/auth/SignupLevel1";
+import SignupLevel2 from "../../components/auth/SignupLevel2";
+import SignupLevel3 from "../../components/auth/SignupLevel3";
+import SignupLevel4 from "../../components/auth/SignupLevel4";
+
+
 const SignupPage = () => {
 
 
     const location = useLocation();
     const navigate = useNavigate();
-    const [nicknamedata, setNicknameData] = useState();
+ 
+    const [levelState, setLevelState] = useState(1);
 
-
-    console.log("여기는 회원가입 페이지!!!! ", location);
-
-    const signupHandler = () => {
-
-        const data = {
-            id : location.state.id,
-            nickname : nicknamedata,
-        }
-
-        auth_axios.post(`/signup`, JSON.stringify(data), {
-            headers: {
-              "Content-Type": `application/json`,
-            },
-          })
-        .then((res) => {
-            console.log("회원가입 성공했서?   ", res);
-            navigate('/');  
-
-        }).catch(function(err){
-            console.log(err);
-        });
-    }
-
-    const typeInputHandler = (e) => {
-        setNicknameData(e.target.value);
+    const levelHandler = (data) => {
+        setLevelState(data);
     };
 
+    useEffect(()=>{}, [levelState]);
 
+    function SignupContent () {
+        if(levelState == 1){
+            return( <><SignupLevel1 levelHandler={levelHandler}/></>)
+        }
+        else if(levelState == 2){
+            return(<><SignupLevel2 levelHandler={levelHandler}/></>)
+        }else if(levelState == 3){
+            return(<><SignupLevel3 levelHandler={levelHandler}/></>)
+        }else{
+            return(<><SignupLevel4 levelHandler={levelHandler}/></>)
+        }
+    
+    }
 
-    return (
-        <Container>
-            <div className="login-html">
-                <input id="tab-1" type="radio" name="tab" className="sign-in" defaultChecked/><label htmlFor="tab-1" className="tab">닉네임</label>
-                <input id="tab-2" type="radio" name="tab" className="sign-up"/><label htmlFor="tab-2" className="tab">경력</label>
-                <div className="login-form">
-                <div className="sign-in-htm">
-                    <div className="group">
-                    <label htmlFor="user" className="label">닉네임</label>
-                    <input id="user" type="text" className="input" onChange={typeInputHandler}/>
-                    </div>
-                    <div className="group">
-                    <input type="submit" className="button" value="다음" onClick={signupHandler}/>
-                    </div>
-                </div>
-                <div className="sign-up-htm">
-                    <div className="group">
-                    <label htmlFor="user" className="label">얼마나 일하셨슴까?</label>
-                    <input id="user" type="text" className="input"/>
-                    </div>
-            
-                    <div className="group">
-                    <input type="submit" className="button" value="등록하기"/>
-                    </div>
-                    
-                </div>
-                </div>
-        </div>
-    </Container>
+    return(
+        <>
+            <SignupContent/>
+        </>
+        
+
     )
+
+
+   
 }
 
 export default SignupPage;
