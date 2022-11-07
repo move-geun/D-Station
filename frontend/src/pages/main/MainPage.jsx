0935b48162cda7caa0cf825e307b39a5979b870b
이동근 <ehdrms1426@gmail.com>
2022-11-07 17:31:36 +0900

    add: tmp main

--- frontend/src/pages/main/MainPage.jsx
+++ frontend/src/pages/main/MainPage.jsx
@@ -106,7 +106,7 @@ const CameraControls = () => {
       ref={controls}
       args={[camera, domElement]}
       autoRotate={true}
-      enableZoom={false}
+      enableZoom={true}
     />
   );
 };
@@ -161,11 +161,135 @@ function Sphere() {
     </mesh>
   );
 }
+function Sphere2() {
+  const { scene, gl } = useThree();
+  // The cubeRenderTarget is used to generate a texture for the reflective sphere.
+  // It must be updated on each frame in order to track camera movement and other changes.
+  const cubeRenderTarget = new WebGLCubeRenderTarget(256, {
+    format: RGBFormat,
+    generateMipmaps: true,
+    minFilter: LinearMipmapLinearFilter,
+  });
+  const cubeCamera = new CubeCamera(1, 1000, cubeRenderTarget);
+  cubeCamera.position.set(0, 0, 0);
+  scene.add(cubeCamera);
+
+  // Update the cubeCamera with current renderer and scene.
+  useFrame(() => cubeCamera.update(gl, scene));
+
+  return (
+    <mesh visible position={[5, 0, 0]} rotation={[0, 0, 0]} castShadow>
+      <directionalLight intensity={0.5} />
+      <sphereGeometry attach="geometry" args={[2, 32, 32]} />
+      <meshBasicMaterial
+        attach="material"
+        envMap={cubeCamera.renderTarget.texture}
+        color="white"
+        roughness={0.1}
+        metalness={1}
+      />
+    </mesh>
+  );
+}
+function Sphere3() {
+  const { scene, gl } = useThree();
+  // The cubeRenderTarget is used to generate a texture for the reflective sphere.
+  // It must be updated on each frame in order to track camera movement and other changes.
+  const cubeRenderTarget = new WebGLCubeRenderTarget(256, {
+    format: RGBFormat,
+    generateMipmaps: true,
+    minFilter: LinearMipmapLinearFilter,
+  });
+  const cubeCamera = new CubeCamera(1, 1000, cubeRenderTarget);
+  cubeCamera.position.set(0, 0, 0);
+  scene.add(cubeCamera);
+
+  // Update the cubeCamera with current renderer and scene.
+  useFrame(() => cubeCamera.update(gl, scene));
+
+  return (
+    <mesh visible position={[-5, 0, 0]} rotation={[0, 0, 0]} castShadow>
+      <directionalLight intensity={0.5} />
+      <sphereGeometry attach="geometry" args={[2, 32, 32]} />
+      <meshBasicMaterial
+        attach="material"
+        envMap={cubeCamera.renderTarget.texture}
+        color="white"
+        roughness={0.1}
+        metalness={1}
+      />
+    </mesh>
+  );
+}
+function Sphere4() {
+  const { scene, gl } = useThree();
+  // The cubeRenderTarget is used to generate a texture for the reflective sphere.
+  // It must be updated on each frame in order to track camera movement and other changes.
+  const cubeRenderTarget = new WebGLCubeRenderTarget(256, {
+    format: RGBFormat,
+    generateMipmaps: true,
+    minFilter: LinearMipmapLinearFilter,
+  });
+  const cubeCamera = new CubeCamera(1, 1000, cubeRenderTarget);
+  cubeCamera.position.set(0, 0, 0);
+  scene.add(cubeCamera);
+
+  // Update the cubeCamera with current renderer and scene.
+  useFrame(() => cubeCamera.update(gl, scene));
+
+  return (
+    <mesh visible position={[0, 5, 0]} rotation={[0, 0, 0]} castShadow>
+      <directionalLight intensity={0.5} />
+      <sphereGeometry attach="geometry" args={[2, 32, 32]} />
+      <meshBasicMaterial
+        attach="material"
+        envMap={cubeCamera.renderTarget.texture}
+        color="white"
+        roughness={0.1}
+        metalness={1}
+      />
+    </mesh>
+  );
+}
+function Sphere5() {
+  const { scene, gl } = useThree();
+  // The cubeRenderTarget is used to generate a texture for the reflective sphere.
+  // It must be updated on each frame in order to track camera movement and other changes.
+  const cubeRenderTarget = new WebGLCubeRenderTarget(256, {
+    format: RGBFormat,
+    generateMipmaps: true,
+    minFilter: LinearMipmapLinearFilter,
+  });
+  const cubeCamera = new CubeCamera(1, 1000, cubeRenderTarget);
+  cubeCamera.position.set(0, 0, 0);
+  scene.add(cubeCamera);
+
+  // Update the cubeCamera with current renderer and scene.
+  useFrame(() => cubeCamera.update(gl, scene));
+
+  return (
+    <mesh visible position={[0, -5, 0]} rotation={[0, 0, 0]} castShadow>
+      <directionalLight intensity={0.5} />
+      <sphereGeometry attach="geometry" args={[2, 32, 32]} />
+      <meshBasicMaterial
+        attach="material"
+        envMap={cubeCamera.renderTarget.texture}
+        color="white"
+        roughness={0.1}
+        metalness={1}
+      />
+    </mesh>
+  );
+}
 const MainPage = () => {
   return (
     <Canvas className="canvas">
       <CameraControls />
       <Sphere />
+      <Sphere2 />
+      <Sphere3 />
+      <Sphere4 />
+      <Sphere5 />
       <SkyBox />
     </Canvas>
   );
