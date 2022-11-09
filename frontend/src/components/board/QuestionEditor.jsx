f3ec0b807f6164f72ebe0d86c5eda06482a370ad
김가흔 <rkgms0712@naver.com>
2022-11-09 15:20:45 +0900

    add:질문페이지_axios

--- frontend/src/components/board/QuestionEditor.jsx
+++ frontend/src/components/board/QuestionEditor.jsx
@@ -16,16 +16,9 @@ import Chip from "@mui/joy/Chip";
 import Radio from "@mui/joy/Radio";
 import RadioGroup from "@mui/joy/RadioGroup";
 import Typography from "@mui/joy/Typography";
+import { useEffect } from "react";
 
 export default function QuestionEditor() {
-  const [selected, setSelected] = React.useState("");
-  const editorRef = useRef();
-  const log = () => {
-    if (editorRef.current) {
-      console.log(editorRef.current.getContent());
-    }
-  };
-
   const blank = {
     marginTop: "20px",
     fontSize: "20px",
@@ -33,27 +26,31 @@ export default function QuestionEditor() {
 
   const userId = useRecoilValue(UserIdState);
   const [titleCreate, setTitleCreate] = useState();
-  const [contentCreate, setContentCreate] = useState();
-  const [tagCreate, setTagCreate] = useState();
+  const editorRef = useRef();
+  const [selected, setSelected] = React.useState("");
 
+  console.log("유저아이디ㅣㅣㅣㅣ  ", userId);
+  useEffect(() => {}, [titleCreate]);
   const titleHandler = (e) => {
     setTitleCreate(e.target.value);
-    console.log(e.target.value);
   };
 
   const contentHandler = (e) => {
-    setContentCreate(e.current.getContent());
+    if (editorRef.current) {
+      const editorCreate = editorRef.current.getContent();
+      console.log(editorCreate);
+    }
   };
 
   const writeQuestion = () => {
     const data = {
       id: userId,
       title: titleCreate,
-      content: contentCreate,
-      tag: tagCreate,
+      content: editorRef.current.getContent(),
+      tag: selected,
     };
     connect_axios
-      .post(`/ask`, JSON.stringify(data))
+      .post(`/ask`, data)
       .then((res) => {
         console.log("업로드 완료");
         console.log(res);
@@ -65,55 +62,53 @@ export default function QuestionEditor() {
 
   return (
     <>
-      <FormControl>
-        <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
-          <Box>
-            <Typography level="h2" fontSize="lg" id="best-movie" mb={2}>
-              분류
-            </Typography>
-            <RadioGroup
-              name="best-movie"
-              aria-labelledby="best-movie"
-              row
-              sx={{ flexWrap: "wrap", gap: 1 }}
-            >
-              {["백엔드😀", "프론트엔드😆", "데브옵스😎", "CS🧷", "기타🎸"].map(
-                (name) => {
-                  const checked = selected === name;
-                  return (
-                    <Chip
-                      key={name}
-                      variant={checked ? "soft" : "plain"}
+      <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
+        <Box>
+          <Typography level="h2" fontSize="lg" id="best-movie" mb={2}>
+            분류
+          </Typography>
+          <RadioGroup
+            name="best-movie"
+            aria-labelledby="best-movie"
+            row
+            sx={{ flexWrap: "wrap", gap: 1 }}
+          >
+            {["백엔드😀", "프론트엔드😆", "데브옵스😎", "CS🧷", "기타🎸"].map(
+              (name) => {
+                const checked = selected === name;
+                return (
+                  <Chip
+                    key={name}
+                    variant={checked ? "soft" : "plain"}
+                    color={checked ? "primary" : "neutral"}
+                    startDecorator={
+                      checked && (
+                        <CheckIcon sx={{ zIndex: 1, pointerEvents: "none" }} />
+                      )
+                    }
+                  >
+                    <Radio
+                      variant="outlined"
                       color={checked ? "primary" : "neutral"}
-                      startDecorator={
-                        checked && (
-                          <CheckIcon
-                            sx={{ zIndex: 1, pointerEvents: "none" }}
-                          />
-                        )
-                      }
-                    >
-                      <Radio
-                        variant="outlined"
-                        color={checked ? "primary" : "neutral"}
-                        disableIcon
-                        overlay
-                        label={name}
-                        value={name}
-                        checked={checked}
-                        onChange={(event) => {
-                          if (event.target.checked) {
-                            setSelected(name);
-                          }
-                        }}
-                      />
-                    </Chip>
-                  );
-                }
-              )}
-            </RadioGroup>
-          </Box>
+                      disableIcon
+                      overlay
+                      label={name}
+                      value={name}
+                      checked={checked}
+                      onChange={(event) => {
+                        if (event.target.checked) {
+                          setSelected(name);
+                        }
+                      }}
+                    />
+                  </Chip>
+                );
+              }
+            )}
+          </RadioGroup>
         </Box>
+      </Box>
+      <FormControl>
         <FormLabel style={blank}>제목</FormLabel>
         <Textarea
           placeholder="제목을 입력해주세요."
