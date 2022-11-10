80839ab7dec4a34dfc7e05e50617a2432fcbbb00
이동근 <ehdrms1426@gmail.com>
2022-11-10 09:20:50 +0900

    add:main

---
+++ frontend/src/components/main/StudyContent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import KeyboardDoubleArrowUpOutlinedIcon from "@mui/icons-material/KeyboardDoubleArrowUpOutlined";
+import { StudyWraper } from "./StudyContent.style";
+
+const StudyContent = () => {
+  return (
+    <StudyWraper>
+      <KeyboardDoubleArrowUpOutlinedIcon />
+    </StudyWraper>
+  );
+};
+
+export default StudyContent;
