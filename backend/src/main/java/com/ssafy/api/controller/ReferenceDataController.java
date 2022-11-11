d5cfb78bec178eb8e4d8e4a8507aa36378ed8fe4
김유정 <jeong746@naver.com>
2022-11-11 15:13:13 +0900

    feat : get 참고자료 list

--- backend/src/main/java/com/ssafy/api/controller/ReferenceDataController.java
+++ backend/src/main/java/com/ssafy/api/controller/ReferenceDataController.java
@@ -1,12 +1,17 @@
 package com.ssafy.api.controller;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.api.response.user.UserRes;
+import com.ssafy.api.response.ReferenceData.ReferenceDataRes;
+import com.ssafy.api.response.til.SatelliteTILRes;
+import com.ssafy.api.service.ReferenceDataService;
 
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -21,17 +26,21 @@ import io.swagger.annotations.ApiResponses;
 @RestController
 @RequestMapping("/api/reference")
 public class ReferenceDataController {
-	// @Autowired
-	// UserService userService;
+	 @Autowired
+	 ReferenceDataService referenceDataService;
 
 	@GetMapping("")
 	@ApiOperation(value = "참고자료 검색", notes = "<strong>미션 uid로 참고자료 목록을</strong> 반환한다.")
 	@ApiResponses({ @ApiResponse(code = 200, message = "성공"), @ApiResponse(code = 401, message = "인증 실패"),
 			@ApiResponse(code = 404, message = "사용자 없음"), @ApiResponse(code = 500, message = "서버 오류") })
-	public ResponseEntity<UserRes> getReferenceDataByMUid(
+	public ResponseEntity<ArrayList<ReferenceDataRes>> getReferenceDataByMUid(
 			@RequestParam @ApiParam(value = "mUid 정보", required = true) long uid) {
-
-		return null;
+		ArrayList<ReferenceDataRes> list = referenceDataService.getReferenceDataByMUid(uid);
+		if (list == null) {
+			return ResponseEntity.status(401).body(list);
+		} else {
+			return ResponseEntity.status(200).body(list);
+		}
 	}
 
 }
