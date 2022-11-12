273bc9c1bdc47ea9e3c640edd1278ca7dce3466a
김가흔 <rkgms0712@naver.com>
2022-11-13 00:08:58 +0900

    add:질문페이지_댓글조회

--- frontend/src/components/board/Comment.jsx
+++ frontend/src/components/board/Comment.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { Container, Content, Tag } from "./Comment.style";
+import { Container, Contents, Tag } from "./Comment.style";
 
-const Comment = () => {
+const Comment = ({ Nickname, Content }) => {
   return (
     <Container>
-      <Content>댓글 내용 들어갈 자리</Content>
-      <Tag>글쓴이/댓글단시간</Tag>
+      <Contents>{Content}</Contents>
+      <Tag>{Nickname}</Tag>
     </Container>
   );
 };
