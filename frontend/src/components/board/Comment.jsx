546971b8b52105beca3a90a1bb19a4d88ccbee9a
김가흔 <rkgms0712@naver.com>
2022-11-14 14:49:24 +0900

    fix:질문페이지_접근제한

--- frontend/src/components/board/Comment.jsx
+++ frontend/src/components/board/Comment.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Contents, Name, ButtonGroup } from "./Comment.style";
 import http from "../../api/http";
+import isAuthenticated from "../../api/isAuthenticated";
 import { getUserId } from "../../api/JWT";
 
 const Comment = ({ Uid, Nickname, Content }) => {
@@ -21,12 +22,14 @@ const Comment = ({ Uid, Nickname, Content }) => {
     <Container>
       <Contents>{Content}</Contents>
       <Name>{Nickname}</Name>
-      <ButtonGroup>
-        <button className="modify">수정</button>
-        <button className="delete" onClick={del}>
-          삭제
-        </button>
-      </ButtonGroup>
+      {isAuthenticated() ? (
+        <ButtonGroup>
+          <button className="modify">수정</button>
+          <button className="delete" onClick={del}>
+            삭제
+          </button>
+        </ButtonGroup>
+      ) : null}
     </Container>
   );
 };
--- frontend/src/pages/board/QuestionDetail.jsx
+++ frontend/src/pages/board/QuestionDetail.jsx
@@ -12,6 +12,7 @@ import {
 import CommentEditor from "../../components/board/CommentEditor";
 import CommentDetail from "../../components/board/Comment";
 import http from "../../api/http";
+import isAuthenticated from "../../api/isAuthenticated";
 import { getUserId } from "../../api/JWT";
 
 const QuestionDetail = () => {
@@ -26,6 +27,7 @@ const QuestionDetail = () => {
   const [content, setContent] = useState();
   const [id, setId] = useState();
   const [comments, setComments] = useState();
+  const [checkId, setCheckId] = useState();
 
   useEffect(() => {
     const Uid = location.state.id.Uid;
@@ -37,6 +39,7 @@ const QuestionDetail = () => {
       setTag(res.data.tag);
       setContent(res.data.content);
       setId(res.data.uid);
+      setCheckId(res.data.userId);
     });
     http.connect_axios.get(`reply/?jisickinUid=${Uid}`).then((res) => {
       setComments(res.data.list);
@@ -75,14 +78,18 @@ const QuestionDetail = () => {
       <Content>{content}</Content>
       {/* 버튼그룹 */}
       <Buttons>
-        <Link to="/questionModify" state={{ id: { id } }}>
-          <Button style={{ color: "yellow" }} onClick={modify}>
-            수정
-          </Button>
-        </Link>
-        <Button style={{ color: "orangered" }} onClick={del}>
-          삭제
-        </Button>
+        {isAuthenticated() && (userId==checkId) ? (
+          <>
+            <Link to="/questionModify" state={{ id: { id } }}>
+              <Button style={{ color: "yellow" }} onClick={modify}>
+                수정
+              </Button>
+            </Link>
+            <Button style={{ color: "orangered" }} onClick={del}>
+              삭제
+            </Button>
+          </>
+        ) : null}
         <Button style={{ color: "yellowgreen" }}>
           <Link
             to="/questionlist"
@@ -108,7 +115,7 @@ const QuestionDetail = () => {
       ) : (
         <div>댓글이 없습니다😥.</div>
       )}
-      <CommentEditor uid={id} />
+      {isAuthenticated() ? <CommentEditor uid={id} /> : null}
     </Container>
   );
 };
