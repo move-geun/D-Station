43a84cd98411a99482b8b2198c1b3f50785f9bfa
KimDaeyeong <daeyoung4088@hanmail.net>
2022-11-14 14:37:57 +0900

    fix: userId 추가

--- backend/src/main/java/com/ssafy/api/response/reply/ReplyRes.java
+++ backend/src/main/java/com/ssafy/api/response/reply/ReplyRes.java
@@ -14,6 +14,9 @@ public class ReplyRes {
     @ApiModelProperty(name = "답변 정보")
     Long uid;
 
+    @ApiModelProperty(name = "작성자 아이디")
+    String userId;
+
     @ApiModelProperty(name = "답변자")
     String nickname;
 
@@ -23,6 +26,7 @@ public class ReplyRes {
     public static ReplyRes of(Reply reply) {
         ReplyRes res = new ReplyRes();
         res.setUid(reply.getUid());
+        res.setUserId(reply.getUser().getId());
         res.setNickname(reply.getUser().getNickname());
         res.setContent(reply.getContent());
         return res;
