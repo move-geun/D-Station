117fedffa464c7b1980984a3e4702ec9684a1bf1
김유정 <jeong746@naver.com>
2022-11-16 15:59:09 +0900

    fix : 퀴즈 정답 Post로 수정

--- backend/src/main/java/com/ssafy/api/controller/QuizController.java
+++ backend/src/main/java/com/ssafy/api/controller/QuizController.java
@@ -3,6 +3,7 @@ package com.ssafy.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -38,7 +39,7 @@ public class QuizController {
 		return ResponseEntity.status(200).body(QuizRes.of(quiz));
 	}
 
-	@GetMapping("/correct")
+	@PostMapping("/correct")
 	@ApiOperation(value = "quiz 정답인지 아닌지 확인", notes = "<strong>퀴즈 uid와 사용자가 택한 정답을 보내면 결과를</strong> 반환한다.")
 	@ApiResponses({ @ApiResponse(code = 200, message = "성공"), @ApiResponse(code = 401, message = "인증 실패"),
 			@ApiResponse(code = 404, message = "사용자 없음"), @ApiResponse(code = 500, message = "서버 오류") })
