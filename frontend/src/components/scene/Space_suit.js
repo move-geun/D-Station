5b7ce0672b8f3586455b01671bf4f160d2510667
이동근 <ehdrms1426@gmail.com>
2022-11-17 11:03:26 +0900

    add : tmp main

--- frontend/src/components/scene/Space_suit.js
+++ frontend/src/components/scene/Space_suit.js
@@ -1,11 +1,20 @@
 import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
 export function SpaceSuit(props) {
   const { nodes, materials } = useGLTF("/glb/space_suit.glb");
+  const myMesh = React.useRef();
+  useFrame(({ clock }) => {
+    const a = clock.getElapsedTime() / 10;
+    myMesh.current.rotation.y = a;
+    myMesh.current.position.x = 700 * (Math.sin(a) * 0.1);
+    myMesh.current.position.y = 700 * (Math.cos(a) * 0.1);
+    myMesh.current.position.z = 700 * (Math.sin(a) * 0.1);
+  });
   return (
-    <group {...props} dispose={null}>
-      <group rotation={[-3.09, -0.39, 0.02]}>
+    <group {...props} dispose={null} ref={myMesh}>
+      <group rotation={[3.09, 0, 0.02]}>
         <mesh
           castShadow
           receiveShadow
--- frontend/src/components/scene/TestFront.js
+++ frontend/src/components/scene/TestFront.js
@@ -1,19 +1,43 @@
-import React, { useRef } from "react";
-import { useGLTF } from "@react-three/drei";
+import React, { useRef, useState } from "react";
+import { useGLTF, Html } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-
+import { FaMapMarkerAlt } from "react-icons/fa";
 import { Earth } from "./Earth";
 import { Jupiter } from "./Jupiter";
 import { AstronautOne } from "./Astronaut_1";
 import { StylezedPlanet } from "./Stylezed_planet";
 import { RoundStar } from "./Round_star";
+import { Openmap, Opennews } from "../../recoil/atoms";
+import { useRecoilValue } from "recoil";
+
+function Marker({ children, ...props }) {
+  const [occluded, occlude] = useState();
+
+  return (
+    <Html
+      transform
+      occlude
+      onOcclude={occlude}
+      style={{
+        transition: "all 0.2s",
+        opacity: occluded ? 0 : 1,
+        transform: `scale(${occluded ? 0.25 : 1})`,
+      }}
+      {...props}
+    >
+      {children}
+    </Html>
+  );
+}
 
 export function TestFront(props) {
+  const ismapopen = useRecoilValue(Openmap);
+  const isnewsopen = useRecoilValue(Opennews);
   const myMesh = React.useRef();
   useFrame(({ clock }) => {
     const a = clock.getElapsedTime() / 3;
     myMesh.current.rotation.y = a;
-    myMesh.current.position.x = 300;
+    myMesh.current.position.y = -30;
     // myMesh.current.position.x = 200 * (Math.sin(a) * 0.005);
     // myMesh.current.position.z = 200 * (Math.cos(a) * 0.005);
   });
@@ -26,6 +50,21 @@ export function TestFront(props) {
         <AstronautOne />
         <RoundStar />
       </group>
+      {ismapopen || isnewsopen ? null : (
+        <Marker rotation={[0, 0, 0]} position={[-45, -100, 0]}>
+          <div
+            style={{
+              position: "absolute",
+              fontSize: 800,
+              letterSpacing: -0.5,
+              left: 800,
+            }}
+          >
+            Frontend
+          </div>
+          <FaMapMarkerAlt style={{ color: "indianred" }} fontSize={800} />
+        </Marker>
+      )}
     </group>
   );
 }
