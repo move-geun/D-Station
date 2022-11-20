0c85267a1d50031296261a305f51d873634f80c4
KimDaeyeong <daeyoung4088@hanmail.net>
2022-11-20 21:54:06 +0900

    fix: cors modified

--- backend/src/main/java/com/ssafy/config/WebMvcConfig.java
+++ backend/src/main/java/com/ssafy/config/WebMvcConfig.java
@@ -8,6 +8,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CommonsRequestLoggingFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -16,47 +17,10 @@ import javax.servlet.Filter;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        // configuration.addAllowedOrigin("*");
-        configuration.addAllowedOriginPattern("*");
-        configuration.addAllowedMethod("*");
-        configuration.addAllowedHeader("*");
-        configuration.addExposedHeader(JwtTokenUtil.HEADER_STRING);
-        configuration.setAllowCredentials(true);
-        configuration.setMaxAge(3600L);
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
-    }
-
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-    		registry.addResourceHandler("/resources/**")
-    				.addResourceLocations("/WEB-INF/resources/");
-    		
-    		registry.addResourceHandler("swagger-ui.html")
-    				.addResourceLocations("classpath:/META-INF/resources/");
-
-    		registry.addResourceHandler("/webjars/**")
-    				.addResourceLocations("classpath:/META-INF/resources/webjars/");
-    		
-    		/*
-    		 * 
-    		 * Front-end에서 참조하는 URL을 /dist로 매핑
-    		 * 
-    		 */
-        registry.addResourceHandler("/css/**")
-        			.addResourceLocations("classpath:/dist/css/");
-        	registry.addResourceHandler("/fonts/**")
-        			.addResourceLocations("classpath:/dist/fonts/");
-        registry.addResourceHandler("/icons/**")
-				.addResourceLocations("classpath:/dist/icons/");
-        registry.addResourceHandler("/img/**")
-			.addResourceLocations("classpath:/dist/img/");
-        registry.addResourceHandler("/js/**")
-				.addResourceLocations("classpath:/dist/js/");
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000", "http://k7e106.p.ssafy.io");
     }
 
     public Filter requestLoggingFilter() {
